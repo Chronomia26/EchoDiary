c4de257940e94bfa166bb25baabdf74c1febd1d3
spooder983 <spooder983@gmail.com>
2025-05-22 05:04:52 +0800

    napagana ko na rin yung edit/delete ng diary, ukininam

--- app/src/main/java/com/bigo143/echodiary/DiaryDatabase.java
+++ app/src/main/java/com/bigo143/echodiary/DiaryDatabase.java
@@ -5,7 +5,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {DiaryEntry.class}, version = 3)
+@Database(entities = {DiaryEntry.class}, version = 2)
 // Version 2 and 3 has the ones without Id
 public abstract class DiaryDatabase extends RoomDatabase {
     private static DiaryDatabase instance;
