ff8b603b2a933643aa3b03c59102ecfac52f4139
spooder983 <spooder983@gmail.com>
2025-06-03 17:44:21 +0800

    may profile na

--- app/src/main/java/com/bigo143/echodiary/MainActivity.java
+++ app/src/main/java/com/bigo143/echodiary/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -56,12 +57,27 @@ public class MainActivity extends AppCompatActivity {
         setupKeyboardVisibilityListener();
 
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false); // Remove default title
+
+
+        View customToolbar = getLayoutInflater().inflate(R.layout.toolbar_custom, null);
+        toolbar.addView(customToolbar);
+
+        ImageView profileIcon = customToolbar.findViewById(R.id.toolbar_profile);
+        TextView profileName = customToolbar.findViewById(R.id.toolbar_title);
+
+        // TODO: Load real profile data
+        profileName.setText("John Doe"); // Replace with user's name
+
+        profileIcon.setOnClickListener(v -> drawerLayout.openDrawer(Gravity.LEFT));
+
+
 
         // Set up navigation drawer
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+        /* ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
         drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
+        toggle.syncState(); */
 
         // Load default fragment on first launch
         if (savedInstanceState == null) {
@@ -116,6 +132,8 @@ public class MainActivity extends AppCompatActivity {
             return new SettingsFragment();
         } else if (itemId == R.id.nav_about) {
             return new AboutFragment();
+        } else if (itemId == R.id.nav_profile) {
+            return new ProfileFragment();
         } else {
             return null;
         }
